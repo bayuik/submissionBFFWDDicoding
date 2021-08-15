8a30dfc981318ce50374398145f7aebc74b512c3
bayuik <bayuindrakusuma05.com>
2021-08-15 15:08:11 +0700

    add detail and image

--- src/script/component/pokemon-item.js
+++ src/script/component/pokemon-item.js
@@ -8,13 +8,24 @@ class PokemonItem extends HTMLElement {
     this.render();
 }
 
+  set picture(picture){    
+    this._picture = picture
+    this.render()
+  }
+
+  set detail(detail){
+    this._detail = detail;
+    this.render()
+  }
+
   render(){
+
     this.innerHTML = `
       <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${this._picture}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${this._pokemon.name}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text">Moves: ${this._detail}</p>
           <a href="${this._pokemon.url}" class="btn btn-primary">Details</a>
         </div>
       </div>`
--- src/script/component/pokemon-list.js
+++ src/script/component/pokemon-list.js
@@ -11,6 +11,15 @@ class PokemonList extends HTMLElement {
         this._pokemons.forEach(pokemon => {
             const pokemonItemElement = document.createElement("pokemon-item");
             pokemonItemElement.pokemon = pokemon;
+            fetch(pokemon.url)
+              .then(response => {
+                response.json()
+                .then(pokemon => {
+                  // let data = pokemon
+                  pokemonItemElement.picture = pokemon.sprites.front_default
+                  pokemonItemElement.detail = pokemon.moves[0].move.name
+                })
+              })
             this.appendChild(pokemonItemElement);
         })
     }
